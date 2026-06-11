d58414351bba61a13e475599aa63670ed37faaa4
itznipundixit <nipundixit7317@gmail.com>
2026-06-11 12:15:40 +0530

    Docker

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -172,7 +172,7 @@ export default function Footer() {
             <div className="text-center md:text-left">
               <p className="text-sm text-gray-500">
                 © {currentYear} UECO Entertainment Pvt. Ltd. All rights
-                reserved. Dev Dixit
+                reserved. Dev Dixit Docker
               </p>
             </div>
 
